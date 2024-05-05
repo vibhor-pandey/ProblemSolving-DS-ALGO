b3e4c5542a4f0eb1c0346c9c0f6de04615a392b2
Vibhor Pandey <vibhor.vibhu@gmail.com>
2024-05-05 19:33:18 +0530

    Diameter of Binary Tree

---
+++ Trees-And-Graphs/Trees/Diameter-of-Binary-Tree/Solution-1-DFS.java
@@ -0,0 +1,53 @@
+/**
+ * 106 / 106 test cases passed.
+ * Status: Accepted
+ * Runtime: 0 ms
+ * Memory Usage: 44.4 MB
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int diameter = 0;
+
+    /**
+     * 1) diameter = longestPath(left) + longestPath(right)
+     * @param root
+     * @return
+     */
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root.left == null && root.right == null) return 0;
+        longPath(root);
+        return diameter;
+    }
+
+    private int longPath(TreeNode node) {
+        if(node == null) return 0;
+
+        if(node.left == null && node.right == null) return 1;
+
+        int left = longPath(node.left);
+        int right = longPath(node.right);
+
+        diameter = Math.max(diameter, (left + right));
+
+        return Math.max(left, right) + 1;
+    }
+}
---
+++ Trees-And-Graphs/Trees/Max-Diff-Between-Node-Ancestor/Solution-1-Recursive-Optimized.java
@@ -0,0 +1,41 @@
+/**
+ * 30 / 30 test cases passed.
+ * Status: Accepted
+ * Runtime: 0 ms
+ * Memory Usage: 41.2 MB
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int max = Integer.MIN_VALUE;
+    public int maxAncestorDiff(TreeNode root) {
+        traverse(root, root.val, root.val);
+        return max;
+    }
+
+    private void traverse(TreeNode root, int maxSoFar, int minSoFar) {
+        if(root == null) return;
+        int diff = Math.max(Math.abs(maxSoFar - root.val), Math.abs(minSoFar - root.val));
+        max = Math.max(max, diff);
+        traverse(root.left, Math.max(maxSoFar, root.val), Math.min(minSoFar, root.val));
+        traverse(root.right, Math.max(maxSoFar, root.val), Math.min(minSoFar, root.val));
+    }
+}
