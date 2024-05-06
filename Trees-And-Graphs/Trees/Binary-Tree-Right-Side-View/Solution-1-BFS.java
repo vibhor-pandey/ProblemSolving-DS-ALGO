905cd102873b7831cb3c2ed1c1d30a99e1ba2a12
Vibhor Pandey <vibhor.vibhu@gmail.com>
2024-05-06 22:38:54 +0530

    Binary Tree - Side View

---
+++ Trees-And-Graphs/Trees/Binary-Tree-Right-Side-View/Solution-1-BFS.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        List<Integer> ans = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            int nodesInLevel = queue.size();
+
+            for(int i = 0; i < nodesInLevel; i++) {
+                TreeNode current = queue.poll();
+
+                if(i == (nodesInLevel - 1)) {
+                    ans.add(current.val);
+                }
+                if(current.left != null) {
+                    queue.offer(current.left);
+                }
+                if(current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+        }
+        return ans;
+    }
+}
